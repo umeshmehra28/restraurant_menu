1b2e33d9ff103a965ebdb1e1f485f1d0e82f7b0c
Umesh <umesh2eight@gmail.com>
2022-05-20 13:57:06 +0530

    update

--- src/component/menuApi.js
+++ src/component/menuApi.js
@@ -1,7 +1,8 @@
 const Menu = [
   {
     id: 1,
-    image: "images/maggi.jpg",
+    image:
+      "https://im.rediff.com/getahead/2020/sep/29burnt-garlic-chilli-maggi.jpg",
     name: "maggi",
     category: "breakfast",
     price: "12₹",
@@ -11,8 +12,9 @@ const Menu = [
 
   {
     id: 2,
-    image: "images/allupakoida.jpg",
-    name: "allu pakoida",
+    image:
+      "https://www.mirchitales.com/wp-content/uploads/2019/03/Aloo-Pakora-Recipe-1.jpg",
+    name: "allu pakoda",
     category: "evening",
     price: "20₹",
     description:
@@ -20,7 +22,8 @@ const Menu = [
   },
   {
     id: 3,
-    image: "images/corn.jpg",
+    image:
+      "https://tmbidigitalassetsazure.blob.core.windows.net/rms3-prod/attachments/37/1200x1200/exps40662_SD153322D04_08_7b.jpg",
     name: "corn",
     category: "breakfast",
     price: "10₹",
@@ -29,7 +32,7 @@ const Menu = [
   },
   {
     id: 4,
-    image: "../images/chola.jpg",
+    image: "https://static.toiimg.com/photo/53314156.cms",
     name: "chola",
     category: "lunch",
     price: "50₹",
@@ -38,7 +41,8 @@ const Menu = [
   },
   {
     id: 5,
-    image: "../images/pizza.jpg",
+    image:
+      "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F9%2F2021%2F06%2F15%2Fmozzarella-pizza-margherita-FT-RECIPE0621.jpg&q=60",
     name: "pizza",
     category: "evening",
     price: "80₹",
@@ -47,8 +51,9 @@ const Menu = [
   },
   {
     id: 6,
-    image: "../images/nonvegthali.jpg",
-    name: "Non-Veg Thali",
+    image:
+      "https://spicytamarind.com.au/wp-content/uploads/2021/01/Non-veg-thali-1.jpg",
+    name: "Veg Thali",
     category: "dinner",
     price: "180₹",
     description:
@@ -56,7 +61,8 @@ const Menu = [
   },
   {
     id: 7,
-    image: "../images/sweet.jpg",
+    image:
+      "https://t4.ftcdn.net/jpg/02/54/92/05/360_F_254920579_xOIyVqIWxgUi0fSQ8FwBC95YlIVZpuCd.jpg",
     name: "Sweets",
     category: "dinner",
     price: "60₹",
@@ -65,7 +71,7 @@ const Menu = [
   },
   {
     id: 8,
-    image: "../images/rajmarice.jpg",
+    image: "https://imgk.timesnownews.com/story/iStock-1214464511.jpg",
     name: "Rajma Rice",
     category: "lunch",
     price: "60₹",
@@ -74,9 +80,40 @@ const Menu = [
   },
   {
     id: 9,
-    image: "../images/samosa.jpg",
+    image:
+      "https://en-media.thebetterindia.com/uploads/2017/01/maxresdefault-1.jpg",
     name: "samaso",
     category: "evening",
+    price: "10₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 10,
+    image:
+      "https://media.istockphoto.com/photos/veg-steam-momo-nepalese-traditional-dish-momo-stuffed-with-vegetables-picture-id1292635321?k=20&m=1292635321&s=612x612&w=0&h=pV3-hV9U_0QVuXJ9xq4u7KRurF-9fQWMYYmDTVlBzgE=",
+    name: "Momos",
+    category: "evening",
+    price: "10₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 11,
+    image:
+      "https://5.imimg.com/data5/JE/EU/MY-64985902/non-veg-special-thali-500x500.png",
+    name: "non-Veg Thali",
+    category: "dinner",
+    price: "10₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 12,
+    image:
+      "https://www.cubesnjuliennes.com/wp-content/uploads/2020/12/Easy-French-Toast-1.jpg",
+    name: " French Toast",
+    category: "breakfast",
     price: "10₹",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
